ec081e70077f9a6157410b96b29d0a25721b3e4d
leo7962 <leonardohernandez.fp@academia.umb.edu.co>
2019-05-06 19:22:45 -0500

    Mejora de codigo

--- main.js
+++ main.js
@@ -10,9 +10,25 @@ class Block {
   }
 
   createHash(){
-    return SHA256(this.index + this.date + this.date);
+    return SHA256(this.index + this.date + this.data);
   }
 }
 
 block = new Block(0, 'Prueba');
 console.log(JSON.stringify(block, null,2));
+
+class blockchain {
+  constructor(genesis){
+    this.chain = [this.createFirstBlock(genesis)];
+  }
+  createFirstBlock(genesis){
+    return new Block(0,genesis);
+  }
+  getLastBlock(){
+    return this.chain[this.chain.length-1];
+  }
+  addBlock(data){
+    let prevBlock = this.getLastBlock();
+    let block = new Block(prevBlock+1, data, prevBlock.hash);
+  }
+}
