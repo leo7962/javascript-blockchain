cc8443bf557c498347ee80210c404f93e2651dcc
leo7962 <leonardohernandez.fp@academia.umb.edu.co>
2019-05-09 00:26:00 -0500

    Se actualiza el código y se agregan tres bloques iniciales.

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
-const SHA256 = require(crypto-js/sha256);
+const SHA256 = require("crypto-js/sha256");
 
 class Block {
-  constructor(index, data, previusHash = ''){
+  constructor(index, data, previusHash = '') {
     this.index = index;
     this.date = new Date();
     this.data = data;
@@ -9,26 +9,36 @@ class Block {
     this.hash = this.createHash();
   }
 
-  createHash(){
+  createHash() {
     return SHA256(this.index + this.date + this.data);
   }
 }
 
 block = new Block(0, 'Prueba');
-console.log(JSON.stringify(block, null,2));
+console.log(JSON.stringify(block, null, 2));
 
 class blockchain {
-  constructor(genesis){
+  constructor(genesis) {
     this.chain = [this.createFirstBlock(genesis)];
   }
-  createFirstBlock(genesis){
-    return new Block(0,genesis);
+  createFirstBlock(genesis) {
+    return new Block(0, genesis);
   }
-  getLastBlock(){
-    return this.chain[this.chain.length-1];
+  getLastBlock() {
+    return this.chain[this.chain.length - 1];
   }
-  addBlock(data){
+  addBlock(data) {
     let prevBlock = this.getLastBlock();
-    let block = new Block(prevBlock+1, data, prevBlock.hash);
+    let block = new Block(prevBlock.index + 1, data, prevBlock.hash);
+    this.chain.push(block);
   }
 }
+
+// block = new Block(0, 'Este es el primer bloque de la cadena');
+// console.log(JSON.stringify(block,null,2));
+
+let naniCoin = new blockchain('info de genesis');
+console.log(JSON.stringify(naniCoin.chain, null, 2));
+naniCoin.addBlock('Este es el segundo bloque');
+naniCoin.addBlock('Este es la información del tercero');
+console.log(JSON.stringify(naniCoin.chain, null, 2));
